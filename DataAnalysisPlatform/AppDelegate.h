6102755b2a621b0e0f1b9a42c2c1e937f09650c5
whn <whn@whntekiiMac.local>
2016-11-11 15:30:43 +0800

    Initial Commit

---
+++ DataAnalysisPlatform/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DataAnalysisPlatform
+//
+//  Created by whn on 2016/11/11.
+//  Copyright © 2016年 whn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DataAnalysisPlatform/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DataAnalysisPlatform
+//
+//  Created by whn on 2016/11/11.
+//  Copyright © 2016年 whn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
